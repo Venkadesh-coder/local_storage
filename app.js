ae334a636e7ea634b12696e4c1c9d825446bef69
Venkadesh-coder <54761484+Venkadesh-coder@users.noreply.github.com>
2020-05-16 22:20:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const submit = document.querySelector('.comment-submit');
+const commentList = document.querySelector('.comments');
+const commentInput = document.querySelector('.comment-input');
+
+function template(data) {
+  commentList.insertAdjacentHTML("beforeend", `
+  <div class="comment flex items-start justify-start">
+      <img class="comment-avatar" src="${data.avatar}" />
+      <div class="flex-1">
+        <h3 class="comment-author">${data.author}</h3>
+        <p class="comment-body">${data.comment}</p>
+      </div>
+    </div>
+  </div>`);
+}
+
+function appendComment (event) {
+
+  const data = {
+    avatar: "ece_img.jpg",
+    comment: commentInput.value,
+    author: "People of ECE"
+  };
+
+  event.preventDefault();
+  if (commentInput.value.length < 1) return;
+  template(data);
+  commentInput.value = "";
+  localStorage.setItem('commentListing', commentList.innerHTML);
+}
+
+submit.addEventListener('click', appendComment, false)
+const saved = localStorage.getItem('commentListing');
+if (saved) {
+  commentList.innerHTML = saved;
+}
